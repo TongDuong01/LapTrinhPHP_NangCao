a1965b8512c8751ea3ae72783529d1accdd31e70
TongDuong01 <linhduong050615@gmail.com>
2026-06-29 14:38:54 +0700

    update 29/06/2026 14:24

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+        
+    </head>
+    <body>
+        <h1>Tong Viet Linh Duong</h1>
+        <h3>0306241270</h3>
+        <h3>CD CNTT 23 WEBD</h3>
+    </body>
+</html>
